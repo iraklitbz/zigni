881662d16e631574a2b378c075635f2f53d12d70
irakli <irakli@foodticket.nl>
2026-02-18 10:32:53 +0100

    Initial Commit

---
+++ zigni/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zigni
+//
+//  Created by Irakli Tavberidze on 18/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zigni/zigniApp.swift
@@ -0,0 +1,17 @@
+//
+//  zigniApp.swift
+//  zigni
+//
+//  Created by Irakli Tavberidze on 18/2/26.
+//
+
+import SwiftUI
+
+@main
+struct zigniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
